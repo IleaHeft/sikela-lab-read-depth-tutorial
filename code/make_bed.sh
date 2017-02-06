9aa5fe1cd077c1282ba35b31dd48cd2c628b0f68
IleaHeft <IleaHeft@users.noreply.github.com>
2017-02-06 13:19:23 -0700

    change threading parameters

--- code/make_bed.sh
+++ code/make_bed.sh
@@ -28,7 +28,7 @@ shift $((OPTIND - 1))
 
 if [ "$pairing" == "paired" ]
 then
-    samtools sort -n -@ 5 -m 5G -o ${output}_sorted.bam $bam
+    samtools sort -n -@ 12 -m 2G -o ${output}_sorted.bam $bam
     bedtools bamtobed -split -bedpe -i ${output}_sorted.bam > $output.bed
     code/merge_pairs.pl $output.bed
 elif [ "$pairing" == "single" ]
